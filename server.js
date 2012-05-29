509831f2f97b4b44222e6043d89a5241ba1f21a0
Jonathan Scott <jonathanscott1989@gmail.com>
2012-05-29 21:15:54 +0100

    Basic layout

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.set('view options', { layout: false });
+app.use('/static', express.static(__dirname + '/static'));
+
+// var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+
+// redis.set('foo', 'bar');
+
+// redis.get('foo', function(err, value) {
+//   console.log('foo is: ' + value);
+// });
+
 app.get('/', function(request, response) {
-	response.send('Hello World!');
+	response.render('index.jade', { title: 'JS Competition' });
 });
 
 var port = process.env.PORT || 3000;
