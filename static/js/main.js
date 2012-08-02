cc884706c7797eec24ceb510f9d826e9539a0f3b
Jonathan Scott <jonathanscott1989@gmail.com>
2012-08-02 21:41:19 +0100

    Use knockout for binding, and store code in localstorage while playing around with it

--- static/js/main.js
+++ static/js/main.js
@@ -1,82 +1,42 @@
-jQuery.fn.serializeObject = function() {
-  var arrayData, objectData;
-  arrayData = this.serializeArray();
-  objectData = {};
+var originalSampleCode = $('#code').text();
 
-  $.each(arrayData, function() {
-    var value;
+// Set up a viewModel for storing the sample code
+var viewModel = new (function() {
+	var self = this;
 
-    if (this.value != null) {
-      value = this.value;
-    } else {
-      value = '';
-    }
-
-    if (objectData[this.name] != null) {
-      if (!objectData[this.name].push) {
-        objectData[this.name] = [objectData[this.name]];
-      }
-
-      objectData[this.name].push(value);
-    } else {
-      objectData[this.name] = value;
-    }
-  });
-
-  return objectData;
-};
-
-
-var games = 0;
-
-/**
- * Start a new game
- */
-function new_game() {
-	games += 1
-	var game = $($("#gameTemplate").render({"game_no": games}));
-	$('#games .accordion').append(game);
-	var game_container = game.find('.accordion-inner');
-	run_settings(game_container);
-	game.find('.accordion-toggle').click();
-}
-
-/**
- * Setup the settings menu for a game
- */
-function run_settings(game_container) {
-	var setting_players = game_container.find('.players');
-	for (var i = 1; i <= config.players; i++) {
-		var player_config = $($("#playerSettingTemplate").render({"player_no": i}));
-		setting_players.append(player_config);
+	if (localStorage && 'code' in localStorage) {
+		self.sampleCode = ko.observable(localStorage['code'])
+	} else {
+		self.sampleCode = ko.observable(originalSampleCode);
 	}
-}
+})();
 
-/**
- * Submit the settings
- */
-$('.settings form').live('submit', function() {
-	var form = $(this);
-	var formData = form.serializeObject();
-	var settings = form.closest('.settings');
-	var game_container = settings.closest('.accordion-inner');
-
-	settings.fadeOut(200, function() {
-		$(this).remove();
-
-		// TODO: Load settings into game
-		setup_game(game_container);
+// Record the changes into localstorage
+viewModel.sampleCode.subscribe(function(newValue) {
+	if (localStorage) {
+		localStorage['code'] = newValue;
+	}
+		if (codeMirror.getValue() != newValue) {
+			// Manually bind codeMirror
+			codeMirror.setValue(newValue)
+		}
 	});
 
-	return false;
-});
+ko.applyBindings(viewModel)
 
-/**
- * Set up the game environment
- */
-function setup_game(game_container) {
-	game_container.closest('.accordion-group').addClass('in-progress');
 
-	run_gamerunner.call(game_container);
-	$(game_container).trigger('setup_game');
+// Set up codemirror
+
+// Bindings don't work so well with codeMirror so we have to do this
+function codeChanged(change) {
+	viewModel.sampleCode(change.getValue());
 }
+
+var codeMirror = CodeMirror.fromTextArea($('#code')[0], {"lineNumbers": true, "onChange": codeChanged});
+
+
+// Basic controls
+$('.competition-reset').live('click', function() {
+	console.log("A");
+	viewModel.sampleCode(originalSampleCode);
+})
