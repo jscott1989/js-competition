7a4e35afd29a4ec40b5eb961bdf14d987f840d77
Jonathan Scott <jonathanscott1989@gmail.com>
2012-08-11 17:42:21 +0100

    Initial layout

---
+++ README.md
@@ -0,0 +1,58 @@
+# Codefeud
+
+## About
+Codefeud is a system for running Javascript coding competitions. It comes with several gametypes built in and it is easy to add your own. Codefeud is very early in development and should not be considered production ready.
+
+## Usage
+Configure the competition in config.yaml, selecting a theme from the built in themes (listed below), and gametype from the build in gametypes (listed below). To run locally, ensure the dependencies are installed (`npm install`) and run app.js (`node app.js`).
+
+### Deploying to Heroku
+TODO
+
+### Deploying elsewhere
+TODO
+
+## Configuration
+TODO
+
+## Built in gametypes
+<table>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Game name</th>
+			<th>Description</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>guess</td>
+			<td>Guess</td>
+			<td>A simple game where the entrant must guess a random number. Used during early development.</td>
+		</tr>
+	</body>
+</table>
+
+## Built in themes
+<table>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Theme name</th>
+			<th>Description</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>basic</td>
+			<td>Basic</td>
+			<td>A plain bootstrap-based theme used during development.</td>
+		</tr>
+	</body>
+</table>
+
+## Creating a gametype
+TODO
+
+## Creating a theme
+TODO
---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2012 Jonathan Scott
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions: The above copyright
+ * notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * This is the entry point to Codefeud coding competition runner
+ *
+ * Run it with node app.js
+ */
+
+var express = require('express');
+var app = module.exports = express.createServer();
+
+require('./config/environment.js')(app, express);
+
+// Include basic pages
+require('./routes/pages.js')(app);
+
+function startServer(port) {
+	app.listen(port);
+}
+
+if (require.main === module) {
+	// Start the server automatically if this is run directly
+	var port = process.env.PORT || 3000;
+	startServer(port);
+}
